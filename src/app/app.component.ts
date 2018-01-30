7d2db55797bb34505c07354d4a4cc9164f6390da
the-algo <anujnmor@gmail.com>
2018-01-30 13:05:53 +0530

    Oil Price changed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -28,8 +28,8 @@ export class AppComponent implements OnInit {
   public state: string;
   public lat: number;
   public long: number;
-  public lati = 28.9001843;
-  public longi = -97.9204604;
+  public lati = 31.7383217;
+  public longi = -101.9890111;
   public previous: number = -1;
   public current: number = -1;
   public isOpen = true;
@@ -51,9 +51,9 @@ export class AppComponent implements OnInit {
 
   public index: number = -1;
 
-  public investment_amount = 850000;
+  public investment_amount = 6300000;
   public nri_percentage = 0.5625;
-  public gross_cash_flow_wo_expenses = 850000;
+  public gross_cash_flow_wo_expenses = 6300000;
 
   public sold_boe = 31612;
   public production_expenses = 0;
@@ -78,7 +78,7 @@ export class AppComponent implements OnInit {
   public oil_price = 60;
   public gas_price = 3;
 
-  public partner_name = "LC GoOpCo Karnes #1, LP";
+  public partner_name = "LC GoOpCo Midland #1, LP";
 
   public well_count: number;
   public latlong = [];
@@ -89,7 +89,7 @@ export class AppComponent implements OnInit {
   public roi_percentage: string = "0%";
   public slider_percentage: string = "0%";
 
-  public partnership_raise = 850000;
+  public partnership_raise = 6300000;
   public partnership_term = 56.25;
 
   constructor(private wellapplicationservices: WellApplicationServices) {
@@ -111,9 +111,6 @@ export class AppComponent implements OnInit {
         this.calculateows();
       }
     );
-    this.hypothetical();
-    this.intializeData();
-
   }
 
   // Get the selected Miles from the Radio Button to set the markers on Map
@@ -123,7 +120,7 @@ export class AppComponent implements OnInit {
       this.latlong.pop();
     }
 
-    
+
     if (miles === "One Mile") {
       this.wellapplicationservices.getOneMileJSON().subscribe(
         JSONlat => {
@@ -144,7 +141,7 @@ export class AppComponent implements OnInit {
           this.roifirsttwelve = "0";
           this.roifirsttwentyfour = "0";
           this.roifirstsixty = "0";
-          this.roilasttwelve = "0";      
+          this.roilasttwelve = "0";
 
           this.well_count = JSONlat.length - 1;
           this.previous = -1;
@@ -178,7 +175,7 @@ export class AppComponent implements OnInit {
           this.roifirsttwelve = "0";
           this.roifirsttwentyfour = "0";
           this.roifirstsixty = "0";
-          this.roilasttwelve = "0";      
+          this.roilasttwelve = "0";
 
           this.well_count = JSONlat.length - 1;
           this.previous = -1;
@@ -211,7 +208,7 @@ export class AppComponent implements OnInit {
           this.roifirsttwelve = "0";
           this.roifirsttwentyfour = "0";
           this.roifirstsixty = "0";
-          this.roilasttwelve = "0";      
+          this.roilasttwelve = "0";
 
           this.well_count = JSONlat.length - 1;
           this.previous = -1;
@@ -597,470 +594,5 @@ export class AppComponent implements OnInit {
   // Setting the Lower Slider Div invisible 
   myOnChange() {
     document.getElementById("tooltipText").style.display = 'none';
-  }
-
-  /*-------------------------------------------------------------------------------------------------------------------*/
-
-  // Hypothetical Price, Production, and Cash Flow Scenarios section calculation
-  public unit_price: number = 1700000;
-  public c: number = 0.1125;
-  public Hypothetical_investment_model: number = 850000;
-  public Estimated_oil_model: number = 50;
-  public Estimated_gas_model: number = 50;
-  public Estimated_oil_price_model: number = 60;
-  public Estimated_gas_price_model: number = 3;
-  public Estimated_Oil_ultimate_recovery_model = 77394;
-  public Estimated_Gas_ultimate_recovery_model = 2705;
-  public Projected_Lifespan: number = 0;
-  public Net_Income_Difference: number = 0;
-  public Actual_Dollars_At_Risk: number = 0;
-  public unit_equivalent_model: number = 0;
-  public Net_Revenue_model: number = 0;
-  public five_percent_of_temp: number = 0;
-  public Potential_Monthly_model: number = 0;
-  public Months_to_model: number = 0;
-  public Total_Potential_model: number = 0;
-
-  public Operating_Expenses_Ratio = 5;
-  public Months_to_cash_flow: number;
-  public roi_percent: string = "0%";
-
-  public unit: string;
-
-  // Calculate Hypothetical Price, Production, and Cash Flow Scenarios
-  hypothetical() {
-    let temp1 = 0, temp2 = 0;
-
-    // Unit Equivalent Model Calculation
-    this.unit_equivalent_model = (this.Hypothetical_investment_model / this.unit_price) * 10;
-    this.findFraction(this.unit_equivalent_model);
-
-    // NRI Percentage Calculation
-    this.Net_Revenue_model = this.c * this.unit_equivalent_model;
-    this.Net_Revenue_model = parseFloat(this.Net_Revenue_model.toPrecision(4));
-
-    // Estimated Monthly Income Calculation
-    temp1 = this.Estimated_oil_model * 30 * this.Estimated_oil_price_model;
-    temp1 = temp1 - (this.tax * temp1);
-    temp2 = this.Estimated_gas_model * 30 * this.Estimated_gas_price_model;
-    temp2 = (temp2 - (this.tax_gas * temp2)) * this.Net_Revenue_model;
-    temp1 = temp1 + temp2;
-    temp1 = temp1 - ((this.Operating_Expenses_Ratio / 100) * temp1);
-    this.Potential_Monthly_model = temp1 * this.Net_Revenue_model;
-    this.Potential_Monthly_model = parseFloat(this.Potential_Monthly_model.toString().match(/^-?\d+(?:\.\d{0,2})?/)[0]);
-
-    temp1 = this.Estimated_Oil_ultimate_recovery_model * this.Estimated_oil_price_model;
-    temp1 = temp1 - (this.tax * temp1);
-    temp2 = this.Estimated_Gas_ultimate_recovery_model * this.Estimated_gas_price_model
-    temp2 = temp2 - (this.tax_gas * temp2);
-    temp1 = (temp1 + temp2) * this.Net_Revenue_model;
-    this.Total_Potential_model = temp1 - ((this.Operating_Expenses_Ratio / 100) * temp1);
-
-    // Months to Payout Calculation
-    this.Months_to_model = this.Hypothetical_investment_model / this.Potential_Monthly_model;
-
-    // Estimated Months of Cash Flow
-    this.Months_to_cash_flow = this.Total_Potential_model / this.Potential_Monthly_model;
-
-    this.roi_percent = (Math.floor((this.Total_Potential_model * 100) / this.Hypothetical_investment_model)).toString() + "%";
-  }
-
-  // Converting the Decimal to Fraction Part
-  findFraction(value: number) {
-    let numerator: string = value.toFixed(2).toString();
-    let extra = numerator.split('.');
-
-    if (100 % extra["1"] == 0) {
-      extra["0"] = 100 / extra["1"];
-      extra["1"] = extra["1"] / extra["1"];
-    }
-
-    else {
-      let temp = extra["1"];
-
-      if (temp % (Math.floor(temp / 2)) == 0 && 100 % (Math.floor(temp / 2)) == 0) {
-        extra["1"] = temp / Math.floor(temp / 2);
-        extra["0"] = 100 / Math.floor(temp / 2);
-      }
-
-      else if (temp % 20 == 0 && 100 % 20 == 0) {
-        extra["1"] = temp / 20;
-        extra["0"] = 100 / 20;
-      }
-
-      else if (temp % 10 == 0 && 100 % 10 == 0) {
-        extra["1"] = temp / 10;
-        extra["0"] = 100 / 10;
-      }
-
-      else if (temp % 5 == 0 && 100 % 5 == 0) {
-        extra["1"] = temp / 5;
-        extra["0"] = 100 / 5;
-      }
-
-      else if (temp % 4 == 0 && 100 % 4 == 0) {
-        extra["1"] = temp / 4;
-        extra["0"] = 100 / 4;
-      }
-
-      else if (temp % 2 == 0 && 100 % 2 == 0) {
-        extra["1"] = temp / 2;
-        extra["0"] = 100 / 2;
-      }
-
-      else {
-        extra["1"] = extra["1"];
-        extra["0"] = 100;
-      }
-    }
-
-    if (extra["1"] != 0 && (!isNaN(extra["1"]) || isFinite(extra["0"]))) {
-      if (Math.floor(value) != 0) {
-        if ((extra["1"] && extra["0"]) == 1) {
-          this.unit = Math.floor(value) + extra["1"];
-        }
-        else {
-          this.unit = Math.floor(value) + " " + extra["1"] + "/" + extra["0"];
-        }
-      }
-      else {
-        this.unit = extra["1"] + "/" + extra["0"];
-      }
-    }
-    else {
-      this.unit = Math.floor(this.unit_equivalent_model).toString();
-    }
-  }
-
-  // 2016 K-1 Tax Estimator section calculation
-  public Estimated_Taxable_Income_model: number = 500000;
-  public Investment_Amount_K1_model: number = 0;
-  public Filing_Status: number = -1;
-  public data = [];
-
-  public Tax_Bracket: number = 0;
-  public Tax_Bracket_K1: number = 0;
-  public Estimated_Tax_Due: number = 0;
-  public Deduction_K1: number = 0;
-  public Adjusted_Taxable_Income: number = 0;
-  public Adjusted_Taxable_Income_K1: number = 0;
-
-  public After_Tax_Income: number = 0;
-  public After_Tax_Income_K1: number = 0;
-
-  public Tax_Rate_Pay_Tax: number = 0;
-  public Tax_Rate_Pay_Tax_K1: number = 0;
-
-  public Range_Start: number = 0;
-  public Excess_Income: number = 0;
-  public Excess_Income_K1: number = 0;
-  public Tax_On_Excess: number = 0;
-  public Tax_On_Excess_K1: number = 0;
-  public Estimated_Tax_Due_K1: number = 0;
-  public Adjusted_Net_Income: number = 0;
-  public Adjusted_Net_Income_K1: number = 0;
-  public Tax_Savings_K1: number = 0;
-  public Tax_Liability: number = 0;
-  public Tax_Liability_K1: number = 0;
-  public Partnership_Investment: number = 0;
-  public Income_Difference: number = 0;
-  public k1_deduction_percentage: number = 75;
-  public panel_visit: number = 0;
-
-  // Initialize the data[]
-  intializeData() {
-    this.data[0] = [{
-      "range_start": 191650,
-      "range_end": 416700,
-      "percent_on_excess_k1": 33.0,
-      "percent_on_excess": 33.0,
-      "tax_rate_pay_tax_k1": 46643.75,
-      "tax_rate_pay_tax": 46643.75
-    },
-    {
-      "range_start": 416700,
-      "range_end": 418400,
-      "percent_on_excess_k1": 35.0,
-      "percent_on_excess": 35.0,
-      "tax_rate_pay_tax_k1": 120910.25,
-      "tax_rate_pay_tax": 120910.25
-    },
-    {
-      "range_start": 418400,
-      "range_end": "Infinity",
-      "percent_on_excess_k1": 39.6,
-      "percent_on_excess": 39.6,
-      "tax_rate_pay_tax_k1": 121505.25,
-      "tax_rate_pay_tax": 121505.25
-    }
-    ];
-
-    this.data[1] = [{
-      "range_start": 153100,
-      "range_end": 233350,
-      "percent_on_excess_k1": 28,
-      "percent_on_excess": 28,
-      "tax_rate_pay_tax_k1": 29752.5,
-      "tax_rate_pay_tax": 29752.5
-    },
-    {
-      "range_start": 233350,
-      "range_end": 416700,
-      "percent_on_excess_k1": 33,
-      "percent_on_excess": 33,
-      "tax_rate_pay_tax_k1": 52222.5,
-      "tax_rate_pay_tax": 52222.5
-    },
-    {
-      "range_start": 416700,
-      "range_end": 470700,
-      "percent_on_excess_k1": 35,
-      "percent_on_excess": 35,
-      "tax_rate_pay_tax_k1": 112728,
-      "tax_rate_pay_tax": 112728
-    },
-    {
-      "range_start": 470700,
-      "range_end": "Infinity",
-      "percent_on_excess_k1": 39.6,
-      "percent_on_excess": 39.6,
-      "tax_rate_pay_tax_k1": 131628,
-      "tax_rate_pay_tax": 131628
-    }
-    ];
-
-    this.data[2] = [{
-      "range_start": 116675,
-      "range_end": 208350,
-      "percent_on_excess_k1": 33,
-      "percent_on_excess": 28,
-      "tax_rate_pay_tax_k1": 26111.25,
-      "tax_rate_pay_tax": 26111.25
-    },
-    {
-      "range_start": 208350,
-      "range_end": 235350,
-      "percent_on_excess_k1": 35,
-      "percent_on_excess": 33,
-      "tax_rate_pay_tax_k1": 56364,
-      "tax_rate_pay_tax": 56364
-    },
-    {
-      "range_start": 235350,
-      "range_end": "Infinity",
-      "percent_on_excess_k1": 39.6,
-      "percent_on_excess": 35,
-      "tax_rate_pay_tax_k1": 65814,
-      "tax_rate_pay_tax": 65814
-    }
-    ];
-
-    this.data[3] = [{
-      "range_start": 212500,
-      "range_end": 416700,
-      "percent_on_excess_k1": 33,
-      "percent_on_excess": 28,
-      "tax_rate_pay_tax_k1": 49816,
-      "tax_rate_pay_tax": 26111.25
-    },
-    {
-      "range_start": 416700,
-      "range_end": 444550,
-      "percent_on_excess_k1": 35,
-      "percent_on_excess": 33,
-      "tax_rate_pay_tax_k1": 117202.5,
-      "tax_rate_pay_tax": 117202.5
-    },
-    {
-      "range_start": 444550,
-      "range_end": "Infinity",
-      "percent_on_excess_k1": 39.6,
-      "percent_on_excess": 35,
-      "tax_rate_pay_tax_k1": 126950,
-      "tax_rate_pay_tax": 126950
-    }
-    ];
-  }
-
-  // Get the selected value from the dropdown list
-  getSelected(selectedValue: string) {
-    this.Filing_Status = parseInt(selectedValue);
-  }
-
-  // Calculate the K1 with and without K1 value 
-  /*
-  compare_k1() {
-    let valid : number = 0;
-    if (this.Filing_Status != -1 && this.Investment_Amount_K1_model > 0) {
-
-      this.Deduction_K1 = this.Investment_Amount_K1_model * (this.k1_deduction_percentage / 100);
-      
-      this.Adjusted_Taxable_Income = this.Estimated_Taxable_Income_model;
-      this.Adjusted_Taxable_Income_K1 = this.Estimated_Taxable_Income_model - this.Deduction_K1;
-
-      for (var i = 0; i < this.data[this.Filing_Status].length; i++) {
-        if (this.data[this.Filing_Status][i].range_start <= this.Adjusted_Taxable_Income_K1 && this.Adjusted_Taxable_Income_K1 <= this.data[this.Filing_Status][i].range_end) {
-          valid = 1;
-          this.Tax_Bracket = this.data[this.Filing_Status][i].percent_on_excess / 100;
-          this.Tax_Bracket_K1 = this.data[this.Filing_Status][i].percent_on_excess_k1 / 100;
-          this.Tax_Rate_Pay_Tax = this.data[this.Filing_Status][i].tax_rate_pay_tax;
-          this.Tax_Rate_Pay_Tax_K1 = this.data[this.Filing_Status][i].tax_rate_pay_tax_k1;
-          this.Range_Start = this.data[this.Filing_Status][i].range_start;
-          break;
-        }
-        else{
-          this.Tax_Bracket = 0;
-          this.Tax_Bracket_K1 = 0;
-          this.Tax_Rate_Pay_Tax = 0;
-          this.Tax_Rate_Pay_Tax_K1 = 0;
-          this.Range_Start = 0;
-        }
-      }
-
-      if(valid == 1){
-        this.Excess_Income = this.Adjusted_Taxable_Income - this.Range_Start;
-        this.Excess_Income_K1 = this.Adjusted_Taxable_Income_K1 - this.Range_Start;
-  
-        this.Tax_On_Excess = this.Excess_Income * (this.Tax_Bracket);
-        this.Tax_On_Excess_K1 = this.Excess_Income_K1 * (this.Tax_Bracket_K1);
-  
-  
-        this.Estimated_Tax_Due = this.Tax_On_Excess + this.Tax_Rate_Pay_Tax;
-        this.Estimated_Tax_Due_K1 = this.Tax_On_Excess_K1 + this.Tax_Rate_Pay_Tax_K1;
-  
-  
-        this.Adjusted_Net_Income = this.Adjusted_Taxable_Income - this.Estimated_Tax_Due;
-        this.Adjusted_Net_Income_K1 = this.Adjusted_Taxable_Income_K1 - this.Estimated_Tax_Due_K1;
-  
-        this.Tax_Savings_K1 = this.Estimated_Tax_Due - this.Estimated_Tax_Due_K1;
-  
-        this.Tax_Liability = this.Estimated_Tax_Due;
-        this.Tax_Liability_K1 = this.Estimated_Tax_Due_K1;
-  
-        this.Partnership_Investment = this.Investment_Amount_K1_model;
-  
-        this.Income_Difference = this.Adjusted_Net_Income - this.Adjusted_Net_Income_K1;
-        this.Actual_Dollars_At_Risk = this.Investment_Amount_K1_model - this.Income_Difference;  
-      }
-
-      else{
-        this.Tax_Liability = 0;
-        this.Adjusted_Net_Income = 0;
-        this.Tax_Savings_K1 = 0;
-        this.Tax_Liability_K1 = 0;
-        this.Adjusted_Net_Income_K1 = 0;
-        this.Income_Difference = 0;
-        this.Actual_Dollars_At_Risk = 0;
-      }
-
-    }
-
-    else {
-      if(this.Filing_Status == -1 && this.Investment_Amount_K1_model == 0){
-        alert("Please select Filing Status and enter Investment Amount");        
-      }
-
-      else if (this.Filing_Status == -1) {
-        alert("Please select Filing Status");
-      }
-
-      else {
-        alert("Please enter Investment Amount");
-      }
-    }
-  }
-  */
-
-  compare_k1() {
-    this.Deduction_K1 = this.Investment_Amount_K1_model * 0.75;
-    this.Adjusted_Taxable_Income = this.Estimated_Taxable_Income_model;
-    this.Adjusted_Taxable_Income_K1 = this.Estimated_Taxable_Income_model - this.Deduction_K1;
-
-    for (var i = 0; i < this.data[this.Filing_Status].length; i++) {
-      if (this.data[this.Filing_Status][i].range_start <= this.Adjusted_Taxable_Income && this.Adjusted_Taxable_Income <= this.data[this.Filing_Status][i].range_end) {
-        this.panel_visit = 1;
-
-        this.Tax_Bracket = this.data[this.Filing_Status][i].percent_on_excess / 100;
-        this.Tax_Bracket_K1 = this.data[this.Filing_Status][i].percent_on_excess_k1 / 100;
-        this.Tax_Rate_Pay_Tax = this.data[this.Filing_Status][i].tax_rate_pay_tax;
-        this.Tax_Rate_Pay_Tax_K1 = this.data[this.Filing_Status][i].tax_rate_pay_tax_k1;
-        this.Range_Start = this.data[this.Filing_Status][i].range_start;
-
-        break;
-      }
-    }
-
-    this.Excess_Income = this.Adjusted_Taxable_Income - this.Range_Start;
-    this.Tax_On_Excess = this.Excess_Income * (this.Tax_Bracket);
-    this.Estimated_Tax_Due = this.Tax_On_Excess + this.Tax_Rate_Pay_Tax;
-    this.Adjusted_Net_Income = this.Adjusted_Taxable_Income - this.Estimated_Tax_Due;
-    this.Tax_Liability = this.Estimated_Tax_Due;
-  }
-
-  // Calculate the K1 with value
-  compare_k2() {
-    if (this.panel_visit == 1) {
-      this.Deduction_K1 = this.Investment_Amount_K1_model * (this.k1_deduction_percentage / 100);
-      this.Adjusted_Taxable_Income_K1 = this.Estimated_Taxable_Income_model - this.Deduction_K1;
-
-      for (var i = 0; i < this.data[this.Filing_Status].length; i++) {
-        if (this.data[this.Filing_Status][i].range_start <= this.Adjusted_Taxable_Income_K1 && this.Adjusted_Taxable_Income_K1 <= this.data[this.Filing_Status][i].range_end) {
-          this.Tax_Bracket_K1 = this.data[this.Filing_Status][i].percent_on_excess_k1 / 100;
-          this.Tax_Rate_Pay_Tax_K1 = this.data[this.Filing_Status][i].tax_rate_pay_tax_k1;
-          this.Range_Start = this.data[this.Filing_Status][i].range_start;
-        }
-      }
-
-      this.Excess_Income_K1 = this.Adjusted_Taxable_Income_K1 - this.Range_Start;
-      this.Tax_On_Excess_K1 = this.Excess_Income_K1 * (this.Tax_Bracket_K1);
-      this.Estimated_Tax_Due_K1 = this.Tax_On_Excess_K1 + this.Tax_Rate_Pay_Tax_K1;
-      this.Adjusted_Net_Income_K1 = this.Adjusted_Taxable_Income_K1 - this.Estimated_Tax_Due_K1;
-      this.Tax_Savings_K1 = this.Estimated_Tax_Due - this.Estimated_Tax_Due_K1;
-      this.Tax_Liability_K1 = this.Estimated_Tax_Due_K1;
-      this.Partnership_Investment = this.Investment_Amount_K1_model;
-      this.Income_Difference = this.Adjusted_Net_Income - this.Adjusted_Net_Income_K1;
-      this.Actual_Dollars_At_Risk = this.Investment_Amount_K1_model - this.Income_Difference;
-    }
-  }
-
-  // Changing the Panel Icon
-  onclicktoggle(idname: string) {
-    let icon = "";
-    if (idname === "accordion1") {
-      icon = "icon1";
-    }
-
-    else {
-      icon = "icon2";
-    }
-
-    let cN = document.getElementById(icon).className;
-
-    if (icon === "icon1") {
-
-      if (document.getElementById("icon2").className === "glyphicon glyphicon-minus") {
-        document.getElementById("icon2").className = "glyphicon glyphicon-plus";
-      }
-
-      if (cN === "glyphicon glyphicon-plus") {
-        document.getElementById(icon).className = "glyphicon glyphicon-minus";
-      }
-      else {
-        document.getElementById(icon).className = "glyphicon glyphicon-plus";
-      }
-    }
-
-    else {
-      if (document.getElementById("icon1").className === "glyphicon glyphicon-minus") {
-        document.getElementById("icon1").className = "glyphicon glyphicon-plus";
-      }
-
-      if (cN === "glyphicon glyphicon-plus") {
-        document.getElementById(icon).className = "glyphicon glyphicon-minus";
-      }
-      else {
-        document.getElementById(icon).className = "glyphicon glyphicon-plus";
-      }
-    }
   }
 }
